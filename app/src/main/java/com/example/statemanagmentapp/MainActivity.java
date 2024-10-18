b82de12298eaa8195c728f6aae7a15413b839570
9578989u <adrianzych8@gmail.com>
2024-10-18 12:38:18 +0200

    cos

--- app/src/main/java/com/example/statemanagmentapp/MainActivity.java
+++ app/src/main/java/com/example/statemanagmentapp/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.statemanagmentapp;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.lifecycle.ViewModelProvider;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -15,15 +18,50 @@ public class MainActivity extends AppCompatActivity {
     EditText editText;
     private CountViewModel countViewModel;
     private TextView textViewCount;
+    private Switch switch1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ch=(CheckBox)findViewById(R.id.checkBox);
 
+
+        setContentView(R.layout.activity_main);
+        CheckBox ch = findViewById(R.id.checkBox);
         textViewCount = findViewById(R.id.textViewCount);
         Button buttonIncrement = findViewById(R.id.buttonIncrement);
+        editText = findViewById(R.id.EditText);
+        Switch switch1 = findViewById(R.id.switch1);
+        TextView TextView2 = findViewById(R.id.TextView2);
+
+
+
+        ch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked) {
+                    TextView2.setVisibility(View.VISIBLE);
+                }else {
+                    TextView2.setVisibility(View.GONE);
+                }
+            }
+        });
+
+
+
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                }else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+            }
+        });
+
+
 
         countViewModel = new ViewModelProvider(this) .get(CountViewModel.class);
         updateCountText();
@@ -37,6 +75,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
 
     private void updateCountText() {
         textViewCount.setText("Licznik: " + countViewModel.getCount());
